acba6f0467c057c67a820666cee0d5b31e276347
liammoore44 <liammoore4441@gmail.com>
2020-05-07 20:35:37 +0100

    added buffer to make buys more accurate

--- repeat_winners.py
+++ repeat_winners.py
@@ -8,7 +8,7 @@ path = "C:\\Users\\lm44\\Documents\\Data\\Repeat Winners Research"
 
 list = os.listdir(path)
 
-def weight_and_returns_model(future_pct, pct_change, weight):
+def probability_predictions(future_pct, pct_change, weight):
     incorrect_dont_buy = []
     incorrect_buy = []
     correct_dont_buy = []
@@ -22,6 +22,7 @@ def weight_and_returns_model(future_pct, pct_change, weight):
         df['%change'] = (df['Close'] - df['Open']) / df['Open'] * 100
         future_length = int(len(df) / 100 * future_pct)
         df.dropna(inplace=True)
+        
         if len(df) < 100:
             pass
         else:
@@ -33,6 +34,7 @@ def weight_and_returns_model(future_pct, pct_change, weight):
             df_not_buy = df.loc[df['%change'] < pct_change]
             df.dropna(inplace=True)
             future_df.dropna(inplace=True)
+            mean_change = np.mean(df['w%change'].values)
             mean_buy = np.mean(df_buy['w%change'].values)
             mean_sell = np.mean(df_not_buy['w%change'].values)
             probability_buy = len(df_buy) / len(df)
@@ -40,30 +42,36 @@ def weight_and_returns_model(future_pct, pct_change, weight):
             future_return = (np.cumsum(future_df['%change'].values))
             future_mean = future_return[-1]
             market_return.append(future_mean)
-
-            if (probability_buy*mean_buy) > abs(probability_sell*mean_sell):
+            last_price = df['Close'].iloc[-1]
+            last_3_prices = df['w%change'].iloc[-3:]
+            
+            if ((probability_buy*mean_buy) > abs(probability_sell*mean_sell)) and not all(n < 0 for n in last_3_prices):
                 df['buy or sell'] = 1
             else:
                 df['buy or sell'] = 0
 
             if (df['buy or sell'].values[1] == 1) and (future_mean >= pct_change):
-                print(f'correct buy: {future_mean} holding period:{days} days')
+                print(f'correct buy: {future_mean}')
                 correct_buy.append(future_mean)
                 total_return.append(future_mean)
+#                 print(df)
+            
             elif (df['buy or sell'].values[1] == 0) and (future_mean < pct_change):
-                print(f'correct dont buy: {future_mean}')
+#                 print(f'correct dont buy: {future_mean}')
                 correct_dont_buy.append(future_mean)
-                missed_returns.append(future_mean)
+            
             elif (df['buy or sell'].values[1] == 0) and (future_mean >= pct_change):
-                print(f'incorrect dont buy: {future_mean}')
+#                 print(f'incorrect dont buy: {future_mean}')
                 incorrect_dont_buy.append(future_mean)
                 missed_returns.append(future_mean)
+            
             elif (df['buy or sell'].values[1] == 1) and (future_mean < pct_change):
-                print(f'incorrect buy: {future_mean} holding period:{days} days')
+                print(f'incorrect buy: {future_mean} ')
                 incorrect_buy.append(future_mean)
                 total_return.append(future_mean)
+#                 print(df)
 
-    print(f'incorrect dont buys: {len(incorrect_dont_buy)}', 
+    print(f'\nincorrect dont buys: {len(incorrect_dont_buy)}', 
           f'incorrect buys: {len(incorrect_buy)}', 
           f'correct buys: {len(correct_buy)}',
           f'correct dont buys: {len(correct_dont_buy)}',
@@ -72,4 +80,4 @@ def weight_and_returns_model(future_pct, pct_change, weight):
           f'\nMISSED RETURN :({np.mean(missed_returns)})%.'
          )
 
-weight_and_returns_model(20, 2.5, 1)
+probability_predictions(20, 5, 1)
